6c42f13f827329ae25d8fcff6ea0b4498bdc39cd
Alexander Nguyen <cernvii@gmail.com>
2021-03-02 19:23:19 +0000

    ex 6 and 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Ashen", last_name: "Knight", hourly_rate: 50)
+@store1.employees.create(first_name: "Onion", last_name: "Knight", hourly_rate: 70)
+@store2.employees.create(first_name: "Alexander", last_name: "Nguyen", hourly_rate: 40)
+@store2.employees.create(first_name: "Lira", last_name: "Call", hourly_rate: 80)
+@store2.employees.create(first_name: "Soren", last_name: "Bjerg", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,10 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "give a store name"
+@store_name = gets.chomp
+@store4 = Store.new(name: @store_name)
+puts @new_store.valid?
+puts @store4.errors.messages(:name)
+puts @store4.errors.messages(:annual_revenue)
+puts @store4.errors.messages
